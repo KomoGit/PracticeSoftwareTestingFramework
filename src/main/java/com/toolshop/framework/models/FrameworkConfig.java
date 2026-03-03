84d56e5ca3aca88fef3b09e63bf45175733aad8a
Elnur <63106963+KomoGit@users.noreply.github.com>
2026-03-03 22:56:50 +0400

    Changed boolean name for headless.

--- src/main/java/com/toolshop/framework/models/FrameworkConfig.java
+++ src/main/java/com/toolshop/framework/models/FrameworkConfig.java
@@ -4,9 +4,8 @@ public class FrameworkConfig {
     public String browser;
     public String baseUrl;
     public String videoDir;
-    public boolean headless;
+    public boolean IsHeadless;
     public boolean screenshotOnFailure;
-//    public boolean recordTests;
     public int timeout;
 
     public FrameworkConfig() {}
