0c6ebd6511752220685cb70783bc1f8ee1c946c0
Elnur <63106963+KomoGit@users.noreply.github.com>
2026-03-04 17:24:22 +0400

    TestUser model for Sign In automation

---
+++ src/main/java/com/toolshop/framework/models/TestUser.java
@@ -0,0 +1,12 @@
+package com.toolshop.framework.models;
+
+public class TestUser {
+    public String Name;
+    public String Role;
+    public String Email;
+    public String Password;
+
+    TestUser(){
+
+    }
+}
