6ee6143ecde09c53baf5dcce6bcc4230ed58ae08
Elnur <63106963+KomoGit@users.noreply.github.com>
2026-03-04 17:22:53 +0400

    Ambigious naming fixed.

--- src/main/java/com/toolshop/framework/pages/SignInPage.java
+++ src/main/java/com/toolshop/framework/pages/SignInPage.java
@@ -6,13 +6,13 @@ import com.microsoft.playwright.Page;
 public class SignInPage extends BasePage {
     private final String url = "/auth/login";
 
-    private final Locator emailInput;
-    private final Locator passwordInput;
-    private final Locator loginButton;
-    private final Locator failedErrorBox;
-    private final Locator emailErrorBox;
-    private final Locator passwordErrorBox;
-    private final Locator alertDangerBox;
+    public final Locator emailInput;
+    public final Locator passwordInput;
+    public final Locator loginButton;
+    public final Locator errorBox;
+    public final Locator emailErrorBox;
+    public final Locator passwordErrorBox;
+    public final Locator alertDangerBox;
 
     public final String invalidLoginErrorMessage = "Invalid email or password";
     public final String emailIsRequiredErrorMessage = "Email is required";
@@ -24,7 +24,7 @@ public class SignInPage extends BasePage {
         this.emailInput = page.locator("#email");
         this.passwordInput = page.locator("#password");
         this.loginButton = page.locator("input[data-test='login-submit']");
-        this.failedErrorBox = page.locator("//div[@class='help-block']");
+        this.errorBox = page.locator("//div[@class='help-block']");
         this.emailErrorBox = page.locator("#email-error");
         this.passwordErrorBox = page.locator("#password-error");
         this.alertDangerBox = page.locator(".alert-danger");
@@ -40,21 +40,5 @@ public class SignInPage extends BasePage {
         passwordInput.fill(password);
         loginButton.click();
         return this;
-    }
-
-    public String getErrorFailMessage(){
-        return failedErrorBox.textContent().trim();
-    }
-
-    public String getEmailErrorMessage(){
-        return emailErrorBox.textContent().trim();
-    }
-
-    public String getPasswordErrorMessage(){
-        return passwordErrorBox.textContent().trim();
-    }
-
-    public Locator getAlertDangerBox() {
-        return alertDangerBox;
     }
 }
