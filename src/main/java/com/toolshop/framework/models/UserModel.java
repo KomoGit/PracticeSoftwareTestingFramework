c54c66d2c00ad6c25b9ff8049e3200bfcbe9a783
Elnur <63106963+KomoGit@users.noreply.github.com>
2026-03-14 01:11:31 +0400

    renamed the model

---
+++ src/main/java/com/toolshop/framework/models/UserModel.java
@@ -0,0 +1,19 @@
+package com.toolshop.framework.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserModel {
+    public String Name;
+    public String Role;
+    public String Email;
+    public String Password;
+}
